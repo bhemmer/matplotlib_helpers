64cce72ea22f249bd1b8eb43e281d9d2665a756c
bhemmer <22427415+bhemmer@users.noreply.github.com>
2024-11-08 14:46:04 +0100

    plot_lineset

--- matplotlib_helpers/__init__.py
+++ matplotlib_helpers/__init__.py
@@ -1,4 +1,5 @@
 from .colors import set_lightness
 from .gs import fmt_gs_axs, gs_axs, wvn_and_wvl
+from .lineset import plot_lineset
 
-__all__ = ["gs_axs", "fmt_gs_axs", "wvn_and_wvl", "set_lightness"]
+__all__ = ["gs_axs", "fmt_gs_axs", "wvn_and_wvl", "set_lightness", "plot_lineset"]
